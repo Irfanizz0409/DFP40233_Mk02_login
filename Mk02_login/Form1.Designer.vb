4fe164c7361c1e734f8152242446bcd3593f4dfb
Irfanizz0409 <irfanizz0409@gmail.com>
2024-08-12 11:56:57 +0800

    Add project files.

---
+++ Mk02_login/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtFirst = New TextBox()
+        txtSecond = New TextBox()
+        btnLogin = New Button()
+        lblIdpengguna = New Label()
+        lblLogmasuk = New Label()
+        lblKatalaluan = New Label()
+        SuspendLayout()
+        ' 
+        ' txtFirst
+        ' 
+        txtFirst.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtFirst.Location = New Point(124, 75)
+        txtFirst.Multiline = True
+        txtFirst.Name = "txtFirst"
+        txtFirst.Size = New Size(211, 32)
+        txtFirst.TabIndex = 1
+        ' 
+        ' txtSecond
+        ' 
+        txtSecond.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtSecond.Location = New Point(124, 118)
+        txtSecond.Multiline = True
+        txtSecond.Name = "txtSecond"
+        txtSecond.Size = New Size(211, 32)
+        txtSecond.TabIndex = 2
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.BackColor = SystemColors.ButtonShadow
+        btnLogin.Location = New Point(141, 180)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(94, 29)
+        btnLogin.TabIndex = 3
+        btnLogin.Text = "LOGIN"
+        btnLogin.UseVisualStyleBackColor = False
+        ' 
+        ' lblIdpengguna
+        ' 
+        lblIdpengguna.AutoSize = True
+        lblIdpengguna.Location = New Point(12, 78)
+        lblIdpengguna.Name = "lblIdpengguna"
+        lblIdpengguna.Size = New Size(106, 20)
+        lblIdpengguna.TabIndex = 4
+        lblIdpengguna.Text = "ID PENGGUNA"
+        ' 
+        ' lblLogmasuk
+        ' 
+        lblLogmasuk.Location = New Point(124, 39)
+        lblLogmasuk.Name = "lblLogmasuk"
+        lblLogmasuk.Size = New Size(123, 33)
+        lblLogmasuk.TabIndex = 5
+        lblLogmasuk.Text = "LOG MASUK"
+        lblLogmasuk.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblKatalaluan
+        ' 
+        lblKatalaluan.AutoSize = True
+        lblKatalaluan.Location = New Point(12, 121)
+        lblKatalaluan.Name = "lblKatalaluan"
+        lblKatalaluan.Size = New Size(103, 20)
+        lblKatalaluan.TabIndex = 6
+        lblKatalaluan.Text = "KATA LALUAN"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        BackColor = SystemColors.ControlLight
+        ClientSize = New Size(382, 220)
+        Controls.Add(lblKatalaluan)
+        Controls.Add(lblLogmasuk)
+        Controls.Add(lblIdpengguna)
+        Controls.Add(btnLogin)
+        Controls.Add(txtSecond)
+        Controls.Add(txtFirst)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterParent
+        Text = "Login Form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblIdpengguna As Label
+    Friend WithEvents lblLogmasuk As Label
+    Friend WithEvents lblKatalaluan As Label
+
+End Class
---
+++ Mk02_login/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles txtSecond.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblIdpengguna.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblLogmasuk.Click
+
+    End Sub
+End Class
